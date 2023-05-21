40a6fe49c7a57513d9169ff2a494356a94152665
hungntt <minatosnow@gmail.com>
2023-05-21 11:42:11 +0700

    Reload evaluated images

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-
+import time
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename, redirect
 
@@ -41,19 +41,27 @@ def index():
             xai = request.form.get("explainable_ai")
             category = request.form.get("category")
 
-            # Process the image with the GradCamSegmentation class
-            segmentation_image, cam_image, coco_image = GradCamSegmentation().process_image(image_path=filepath,
-                                                                                            category=category,
-                                                                                            label_path=label_filepath,
-                                                                                            xai=xai)
-            # Save the cam_image result to a file
-            result_filename = 'result_' + filename
+            result_filename = f'result_{xai}_{category}_{filename}'
             result_filepath = os.path.join(app.config['RESULT_FOLDER'], result_filename)
-            cam_image.save(result_filepath)
 
-            segment_filename = 'segment_' + filename
+            segment_filename = f'segment_{category}_{filename}'
             segment_filepath = os.path.join(app.config['RESULT_FOLDER'], segment_filename)
-            segmentation_image.save(segment_filepath)
+
+            start_time = time.time()
+            # Process the image with the GradCamSegmentation class
+            # Check if result_filepath is available or not
+            if not os.path.isfile(result_filepath):
+                segmentation_image, cam_image, coco_image = \
+                    GradCamSegmentation().process_image(image_path=filepath,
+                                                        category=category,
+                                                        label_path=label_filepath,
+                                                        xai=xai)
+                # Save the cam_image result to a file
+                cam_image.save(result_filepath)
+
+                segmentation_image.save(segment_filepath)
+            processing_time = time.time() - start_time
+            print(f'Processing time: {processing_time}')
 
             if coco_image is None:
                 return render_template('result.html', image=filepath, segmentation=segment_filepath,
@@ -63,7 +71,8 @@ def index():
                 coco_filepath = os.path.join(app.config['RESULT_FOLDER'], coco_filename)
                 coco_image.save(coco_filepath)
 
-                return render_template('result.html', image=filepath, segmentation=segment_filepath, result=result_filepath,
+                return render_template('result.html', image=filepath, segmentation=segment_filepath,
+                                       result=result_filepath,
                                        coco_image=coco_filepath)
 
     return render_template('index.html')
